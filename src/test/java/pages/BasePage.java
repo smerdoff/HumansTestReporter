208d1e244ca7137ea61ab070493e07be6ada8968
smerdoff <eugenesmerdov17@gmail.com>
2021-02-03 00:31:07 +0300

    Initial commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,6 @@
+package pages;
+
+public abstract class BasePage {
+    public abstract BasePage openPage(String number);
+    public abstract BasePage isPageOpened();
+}
---
+++ src/test/java/pages/TicketPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class TicketPage extends BasePage {
+
+
+    @Override
+    public BasePage openPage(String number) {
+        return null;
+    }
+
+    @Override
+    public BasePage isPageOpened() {
+        return null;
+    }
+}
